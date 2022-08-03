d2f87450fb5cb1f8a02cb4dc0801296abb82a546
Gennadion <ghammurti@gmail.com>
2022-08-03 14:46:25 +0300

    First custom component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   return (
     <div>
       <h2>My first React Project</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
