88ba3e6d1c1f3a06e9e2273c5279567c44026837
Gennadion <ghammurti@gmail.com>
2022-08-04 19:52:30 +0300

    Cleaned up App comp with a new Expenses comp

---
+++ src/components/Expenses.js
@@ -0,0 +1,32 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+
+  return (
+    <div>
+      <ExpenseItem
+        title={props.array[0].title}
+        amount={props.array[0].amount}
+        date={props.array[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.array[1].title}
+        amount={props.array[1].amount}
+        date={props.array[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.array[2].title}
+        amount={props.array[2].amount}
+        date={props.array[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.array[3].title}
+        amount={props.array[3].amount}
+        date={props.array[3].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default Expenses;
