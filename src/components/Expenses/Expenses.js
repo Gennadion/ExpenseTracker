8a86c74128e98921aff5d06ff27be0e06957eaf4
Gennadion <ghammurti@gmail.com>
2022-08-12 12:05:35 +0300

    Less hard code and dynamic data output

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,39 +1,29 @@
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
-import { useState } from 'react';
+import { useState } from "react";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-};
+  };
 
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter selected={filteredYear} onSelectedYear={filterChangeHandler} />
-        <ExpenseItem
-          title={props.array[0].title}
-          amount={props.array[0].amount}
-          date={props.array[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.array[1].title}
-          amount={props.array[1].amount}
-          date={props.array[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.array[2].title}
-          amount={props.array[2].amount}
-          date={props.array[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.array[3].title}
-          amount={props.array[3].amount}
-          date={props.array[3].date}
-        ></ExpenseItem>
+        <ExpensesFilter
+          selected={filteredYear}
+          onSelectedYear={filterChangeHandler}
+        />
+        {props.array.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
